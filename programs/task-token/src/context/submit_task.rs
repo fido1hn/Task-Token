b94c3e8ac563279588e9a7488ee4d3f5bbc43206
fido1hn <confidoekeanya@gmail.com>
2025-02-23 10:00:51 +0100

    renamed developer to signer in SubmitTask context

--- programs/task-token/src/context/submit_task.rs
+++ programs/task-token/src/context/submit_task.rs
@@ -5,7 +5,7 @@ use crate::{state::Task, Submission};
 #[derive(Accounts)]
 pub struct SubmitTask<'info> {
     #[account(mut)]
-    pub developer: Signer<'info>,
+    pub signer: Signer<'info>,
     #[account(
       seeds = [b"task", task.title.as_bytes(), task.owner.key().as_ref()],
       bump
@@ -13,9 +13,9 @@ pub struct SubmitTask<'info> {
     pub task: Account<'info, Task>,
     #[account(
       init,
-      payer = developer,
+      payer = signer,
       space = 8 + Submission::INIT_SPACE,
-      seeds = [b"submission", developer.key().as_ref(), task.key().as_ref()],
+      seeds = [b"submission", signer.key().as_ref(), task.key().as_ref()],
       bump
     )]
     pub submission: Account<'info, Submission>,
@@ -27,7 +27,7 @@ impl<'info> SubmitTask<'info> {
         // Create a submission account
         self.submission.set_inner(Submission {
             task: self.task.key(),
-            developer: self.developer.key(),
+            developer: self.signer.key(),
             submission_link: link,
             bump: bumps.submission,
         });
