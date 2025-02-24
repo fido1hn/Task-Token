bd8511990e5f7f975e1767964366f20aba542fe6
fido1hn <confidoekeanya@gmail.com>
2025-02-24 21:22:48 +0100

    added payment mint to tests

--- tests/task-token.ts
+++ tests/task-token.ts
@@ -6,6 +6,10 @@ import {
   SendTransactionError,
   SystemProgram,
 } from "@solana/web3.js";
+import {
+  createMint,
+  getOrCreateAssociatedTokenAccount,
+} from "@solana/spl-token";
 import { TaskToken } from "../target/types/task_token";
 
 describe("task-token", () => {
@@ -18,24 +22,27 @@ describe("task-token", () => {
   const program = anchor.workspace.TaskToken as Program<TaskToken>;
   const connection = provider.connection;
 
-  // protocol admin
+  // Protocol admin
   const admin = anchor.web3.Keypair.generate();
 
-  // Config
+  // Config PDA
   const [configPda] = PublicKey.findProgramAddressSync(
     [Buffer.from("config"), admin.publicKey.toBuffer()],
     program.programId
   );
-  // Vault
+  // Config Vault
   const [vaultPda] = PublicKey.findProgramAddressSync(
     [Buffer.from("config"), configPda.toBuffer()],
     program.programId
   );
-  // Mint
+  // Task Token Mint
   const [mintPda] = PublicKey.findProgramAddressSync(
     [Buffer.from("rewards"), configPda.toBuffer()],
     program.programId
   );
+  // Payment Mint:
+  let paymentMint: PublicKey;
+
   // Token program
   const tokenProgram = anchor.utils.token.TOKEN_PROGRAM_ID;
   // System program
@@ -59,6 +66,14 @@ describe("task-token", () => {
       console.log(
         `Success! Check out your TX here: https://explorer.solana.com/tx/${txhash}?cluster=Localnet`
       );
+
+      paymentMint = await createMint(
+        provider.connection,
+        admin,
+        admin.publicKey,
+        null,
+        6
+      );
     } catch (e) {
       console.error(`Oops, something went wrong: ${e}`);
     }
@@ -66,27 +81,26 @@ describe("task-token", () => {
 
   it("Is initialized!", async () => {
     // Add your test here.
-    try {
-      const tx = await program.methods
-        .initialize(150)
-        .accountsPartial({
-          admin: admin.publicKey,
-          config: configPda,
-          vault: vaultPda,
-          taskTokenMint: mintPda,
-          tokenProgram,
-          systemProgram,
-        })
-        .signers([admin])
-        .rpc();
-      console.log("Your transaction signature", tx);
-    } catch (error) {
-      const logs = await new SendTransactionError(error).getLogs(
-        provider.connection
-      );
-      console.log(`log: ${logs}`);
-    }
+    // try {
+    //   const tx = await program.methods
+    //     .initialize(150)
+    //     .accountsPartial({
+    //       admin: admin.publicKey,
+    //       config: configPda,
+    //       vault: vaultPda,
+    //       taskTokenMint: mintPda,
+    //       tokenProgram,
+    //       systemProgram,
+    //     })
+    //     .signers([admin])
+    //     .rpc();
+    //   console.log("Your transaction signature", tx);
+    // } catch (error) {
+    //   console.log(`an error occured: ${error}`);
+    // } finally {
+    // }
     console.log(`commitment: ${connection.commitment}`);
     console.log(`cluster: ${connection.rpcEndpoint}`);
+    console.log(`payment Mint addy: ${paymentMint}`);
   });
 });
