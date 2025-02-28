4b3eef61bb4d19024c715862411bd487891a5044
fido1hn <confidoekeanya@gmail.com>
2025-02-28 16:14:46 +0100

    remote task vault info

--- programs/task-token/src/state/task_vault_info.rs
+++
@@ -1,9 +0,0 @@
-use anchor_lang::prelude::*;
-
-#[account]
-#[derive(InitSpace)]
-pub struct TaskVaultInfo {
-    pub task: Pubkey,
-    pub task_vault_bump: u8,
-    pub task_vault_info_bump: u8,
-}
